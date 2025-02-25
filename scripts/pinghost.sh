6ff829b33c1f201453889c9c581f1a8936edec11
safiy-hagamed <safiyhagamed@gmail.com>
2025-02-25 20:05:18 +0500

    pinghost.sh

--- scripts/pinghost.sh
+++ scripts/pinghost.sh
@@ -3,27 +3,24 @@
 #=====================================
 # Name : Safiy
 # Ping a host to check success/failure and mails it
-# Usage : ./pinghost.sh <example.com>
+# Usage : change the hostsite and email variable to desired value
 #====================================
 
 # Variables
-hostsite=$1
-email_id=safiyhagamed@gmail.com
+hostsite="example.com"
+email_id="example@mail.com"
 
-if [ -z "$hostsite" ]; then
-  echo "Error: No host provided. Usage: ./pinghost.sh <host>"
-  exit 1
-fi
-
-# Function for ping check
-checkping() {
-  ping -qc 5 "$hostsite"
-  if [ $? -ne 0 ]; then
-    echo "$hostsite failed to connect" | mail -s "Report: Host ping error" "$email_id"
-  else
-    echo "$hostsite is reachable." | mail -s "host reachable sy" "$email_id"
-  fi
+check_ping() {
+    if ! ping -qc 2 "$hostsite" >/dev/null 2>&1; then
+        local message="$hostsite is unreachable!"
+        echo "$message" | mail -s "$hostsite Ping Error" "$email_id"
+    fi
 }
 
-# Executing function
-checkping
+# infinte loop for 10 seconds
+while true; do
+    check_ping
+    sleep 10
+done
+
+#add the script to crontab  
